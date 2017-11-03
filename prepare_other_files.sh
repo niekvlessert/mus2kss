cc79037861b890acc111ab08004da6ff0c336d07
Niek Vlessert <niekvlessert@gmail.com>
2017-11-03 16:59:17 +0100

    No need for this script anymore...

--- prepare_other_files.sh
+++
@@ -1,3 +0,0 @@
-#This creates player.tmp files which will be used by the sjasm ASM files. The value of C will make the chip select in the player engine.
-bincut2 -o player_fmpac.tmp -s 7 -p C:1 FST2.BIN
-bincut2 -o player_music_module.tmp -s 7 -p C:0 FST2.BIN
